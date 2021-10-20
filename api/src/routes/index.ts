803b78f5eb38db73f1e155ffbbe174982d38d957
Wellington de Lima <wellington.map@hotmail.com>
2021-10-20 20:56:07 -0300

    create new message

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "../controllers/AuthenticateUserController";
+import { CreateMessageController } from "../controllers/CreateMessageController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const routes = Router();
 
 routes.post("/authenticate", new AuthenticateUserController().handle);
+
+routes.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
 
 export { routes };
